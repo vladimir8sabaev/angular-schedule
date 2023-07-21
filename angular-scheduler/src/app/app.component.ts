df1dfbddb096f09f0c2d54cf1d79a67fd2362c70
vladimir8sabaev <vladimir8sabaev@mail.ru>
2023-07-21 13:08:46 +0300

    feat: add arr change when drop event on other date

--- angular-scheduler/src/app/app.component.ts
+++ angular-scheduler/src/app/app.component.ts
@@ -4,6 +4,7 @@ import {
   CalendarOptions,
   DateSelectArg,
   EventClickArg,
+  EventDropArg,
 } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -25,7 +26,7 @@ export class AppComponent {
     firstDay: 1,
     locale: 'ru',
     plugins: [dayGridPlugin, interactionPlugin],
-    editable: true,
+    eventStartEditable: true,
     selectable: true,
     select: this.handleDateSelect.bind(this),
     selectMirror: true,
@@ -42,22 +43,21 @@ export class AppComponent {
       today: 'Сегодня',
     },
     eventClick: this.handleEventClick.bind(this),
+    eventDrop: this.handleEventDrop.bind(this),
   };
   handleDateSelect(selectInfo: DateSelectArg) {
-    const title = prompt('Please enter a new title for your event');
+    const title = prompt('Введите название нового события');
     const calendarApi = selectInfo.view.calendar;
     calendarApi.unselect();
     if (title) {
       calendarApi.addEvent({
         title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
+        date: selectInfo.startStr,
       });
     }
   }
   handleEventClick(clickInfo: EventClickArg) {
-    const title = prompt('Enter new title:');
+    const title = prompt('Введите новое название для существующего события:');
     const date = clickInfo.event.start;
     let calendarApi = this.calendar.getApi();
     if (title) {
@@ -69,7 +69,31 @@ export class AppComponent {
         )}-${String(date!.getDate()).padStart(2, '0')}`,
       };
       this.scheduleEvents.push(event);
+      this.scheduleEvents = this.scheduleEvents.filter(
+        (item) => item.title !== clickInfo.event.title
+      );
       calendarApi.addEvent(event);
+      clickInfo.event.remove();
+      console.log(this.scheduleEvents);
     }
+  }
+  handleEventDrop(dropInfo: EventDropArg) {
+    this.scheduleEvents = this.scheduleEvents.filter(
+      (item) => item.title !== dropInfo.event.title
+    );
+    let calendarApi = this.calendar.getApi();
+    const title = dropInfo.event.title;
+    const date = dropInfo.event.start;
+    const event = {
+      title: title,
+      date: `${date?.getFullYear()}-${String(date!.getMonth() + 1).padStart(
+        2,
+        '0'
+      )}-${String(date!.getDate()).padStart(2, '0')}`,
+    };
+    dropInfo.event.remove();
+    this.scheduleEvents.push(event);
+    calendarApi.addEvent(event);
+    console.log('date changed');
   }
 }
